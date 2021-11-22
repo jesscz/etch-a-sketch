67d2657538a5b3cc52809e298abe0bbf48290e24
Jessie Chen <notionj.jc@gmail.com>
2021-11-21 20:24:21 -0500

    Change class name on hover in grid

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const container = document.getElementById("container");
 const startBtn = document.getElementById("startBtn");
 
+
 startBtn.addEventListener("click", () => {
     makeGrid(16);
+    startBtn.setAttribute("disabled", "");
 });
 
 function makeGrid(gridSize) {
@@ -12,9 +14,15 @@ function makeGrid(gridSize) {
         for (let j = 0; j < gridSize; j++) {
             let gridCell = document.createElement("div");
             gridCell.className = "gridSquare";
-            gridCell.innerText = (i * gridSize) + j;
+            //gridCell.innerText = (i * gridSize) + j;
+            gridCell.addEventListener("mouseover", (event) => {
+                event.target.classList.add("gridSquareHovered");
+                event.target.classList.remove("gridSquare"); //is this line necessary?
+            });
             gridRow.appendChild(gridCell);
         }
         container.appendChild(gridRow);
     }
 }
+
+  
