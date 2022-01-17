4485082cea8b2ba6d080a9603333748f9380936b
Jessie Chen <notionj.jc@gmail.com>
2022-01-17 15:01:41 -0500

    Add buttons

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const container = document.getElementById("container");
 const leftContainer = document.getElementById("left");
 const rightContainer = document.getElementById("right");
 
-const startBtn = document.getElementById("startBtn");
 const resetBtn = document.createElement("button");
 resetBtn.innerHTML = "Reset"
+const rainbowBtn = document.getElementById("rainbow");
+const bnwBtn = document.getElementById("black");
+let counter = 0;
 
-startBtn.addEventListener("click", () => {
+
+start();
+rightContainer.appendChild(resetBtn);
+
+function start() {
     makeGrid(16);
-    startBtn.setAttribute("disabled", "");
-    rightContainer.appendChild(resetBtn);
-});
+    container.style.cssText = 'border: 3px dashed rgb(230, 215, 241)';
+    //rightContainer.appendChild(resetBtn);
+}
 
 resetBtn.addEventListener("click", () => {
     let userGridSize = prompt("What size should the grid be?");
@@ -27,31 +33,42 @@ resetBtn.addEventListener("click", () => {
 });
 
 function randomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
+// function blackGradation() {
+//     let color = getPropertyValue(gridCell)['backgroundColor'];
+//     let perc = 100;
+//     let color = 'hsl(0, 0%, '+ (perc) +'%';
+//     while  (perc >= 0) {
+//         perc -= 10;
+//     }
+//     return color;
+// }
 
 function makeGrid(gridSize) { 
     for (let i = 0; i < gridSize; i++) {
         let gridRow = document.createElement("div");
         gridRow.className = "row";
+
         for (let j = 0; j < gridSize; j++) {
             let gridCell = document.createElement("div");
             gridCell.className = "gridSquare";
-            //gridCell.innerText = (i * gridSize) + j;
             gridCell.addEventListener("mouseover", (event) => {
+                //event.target.style.backgroundColor = blackGradation();
                 event.target.style.backgroundColor = randomColor();
-                event.target.setAttribute("disabled", "");
+                //event.target.setAttribute("disabled", "");
             });
             gridRow.appendChild(gridCell);
         }
         container.appendChild(gridRow);
     }
 }
+
 
   
